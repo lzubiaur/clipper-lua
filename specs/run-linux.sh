55fa977b087174563746b37a4e2f24b2bd377dcd
lzubiaur <zubiaur.laurent@gmail.com>
2017-07-21 18:03:12 +0200

    Add specs script linux

--- specs/run-linux.sh
+++ specs/run-linux.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 export LUA_PATH="./?.lua;../src/?.lua;;"
 # export LUA_CPATH=""
 # export PATH=""
 
-export LD_LIBRARY_PATH=../bin/linux:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="../bin/linux-64:$LD_LIBRARY_PATH"
 
 pushd specs
 luajit tests.lua $*
